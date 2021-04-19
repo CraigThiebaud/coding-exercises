a0cbed13dde2d7e770c3ca8bbc2e485ee7e73596
Craig Thiebaud <craigthiebaud@gmail.com>
2021-04-19 15:25:06 -0500

    Forgot to stage changes to MarsRoverService in previous commit

--- mars-rover/RoverPhotosApp/NasaAPIService/Services/MarsRoverService.cs
+++ mars-rover/RoverPhotosApp/NasaAPIService/Services/MarsRoverService.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace NasaAPIService.Services
@@ -10,7 +11,17 @@ namespace NasaAPIService.Services
     {
         public RoverImages GetRoverImagesByDate(string date)
         {
-            throw new NotImplementedException();
+            var uriString = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=kKh7bH8p42hBOtScs9upsNirlKGtRGQJ9LkDqz4m";
+            RoverImages result;
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync(new Uri(uriString)).Result;
+                result = new RoverImages()
+                {
+                    Images = new List<string>()
+                };
+            }
+            return result;
         }
     }
 }
