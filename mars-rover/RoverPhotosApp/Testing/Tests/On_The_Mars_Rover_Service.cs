28f418a0ab491351cc79939cdfb5dd9ecfcdea22
Craig Thiebaud <craigthiebaud@gmail.com>
2021-04-19 14:46:11 -0500

    test classes made

---
+++ mars-rover/RoverPhotosApp/Testing/Tests/On_The_Mars_Rover_Service.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NasaAPIService.Services;
+using NUnit.Framework;
+
+namespace Testing.Tests
+{
+    public class On_The_Mars_Rover_Service
+    {
+        protected MarsRoverService service;
+
+        [SetUp]
+        protected virtual void SetUp()
+        {
+
+        }
+    }
+}
---
+++ mars-rover/RoverPhotosApp/Testing/Tests/When_Getting_Rover_Images_By_Date.cs
@@ -0,0 +1,33 @@
+﻿using NasaAPIService.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Testing.Tests
+{
+    public class When_Getting_Rover_Images_By_Date : On_The_Mars_Rover_Service
+    {
+        private RoverImages result;
+
+        [SetUp]
+        protected override void SetUp()
+        {
+
+        }
+
+        public void Because(string date)
+        {
+            result =  service.GetRoverImagesByDate(date);
+        }
+
+        [Test]
+        public void Should_Return_Rover_Images_Model()
+        {
+            Because("");
+            Assert.IsInstanceOf<RoverImages>(result);
+        }
+    }
+}
