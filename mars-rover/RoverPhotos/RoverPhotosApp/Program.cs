ae8352300de63ac03b201656475fe7fb61c5a3d8
Craig Thiebaud <craigthiebaud@gmail.com>
2021-04-20 14:37:11 -0500

    Images now downloading using dates from txt file, app will create a folder "MarsPhotos" in "C:\" directory if it doesnt exist, then download images one date at a time to that directory.

--- mars-rover/RoverPhotos/RoverPhotosApp/Program.cs
+++ mars-rover/RoverPhotos/RoverPhotosApp/Program.cs
@@ -1,6 +1,9 @@
 ﻿using NasaAPIService.Services;
 using System;
-
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
 
 namespace RoverPhotosApp
 {
@@ -9,20 +12,32 @@ namespace RoverPhotosApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            //var fileContent = System.IO.File.ReadAllText("~/dates.txt");
             var lines = System.IO.File.ReadAllLines(@"~\..\..\..\..\..\dates.txt");
             var roverService = new MarsRoverService();
-            foreach(var line in lines)
+            DirectoryInfo info = Directory.CreateDirectory("C:\\MarsPhotos");
+            
+
+            foreach (var line in lines)
             {
                 try
                 {
                     var result = roverService.GetRoverImagesByDate(line);
-                    Console.WriteLine(result);
+                    DownloadImages(result.Images);
+                    Console.WriteLine($"Total images on {line}: {result.Images.Count}");
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine(ex.Message);
                 }
+            }
+        }
+
+        public static void DownloadImages(List<string> imageUrls)
+        {
+            foreach (var url in imageUrls)
+            {
+                var webClient = new WebClient();
+                webClient.DownloadFile(url, "C:\\MarsPhotos\\" + url.Split('/').Last());
             }
         }
     }
