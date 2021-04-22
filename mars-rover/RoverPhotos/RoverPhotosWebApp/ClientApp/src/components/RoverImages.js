fa0c2e7403e24e61c8ab53a3b5f432cf8b6adcc4
Craig Thiebaud <craigthiebaud@gmail.com>
2021-04-22 08:55:38 -0500

    initial commit for react, still has some template code to be removed, also added component for viewing rover images

---
+++ mars-rover/RoverPhotos/RoverPhotosWebApp/ClientApp/src/components/RoverImages.js
@@ -0,0 +1,15 @@
+﻿import React, { Component } from 'react';
+import { Form, Field } from 'react-final-form';
+
+export class RoverImages extends Component {
+    static displayName = RoverImages.name;
+
+    constructor(props) {
+        super(props);
+        this.state = { forecasts: [], loading: true };
+    }
+
+    async getImagesByDate() {
+
+    }
+}
---
+++ mars-rover/RoverPhotos/RoverPhotosWebApp/Models/RoverImages.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoverPhotosWebApp.Models
+{
+    public class RoverImages
+    {
+        public List<string> Images { get; set; }
+    }
+}
